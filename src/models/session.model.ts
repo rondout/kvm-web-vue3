98e641d3c75a1c0be080de35d8e533f6d31f2586
shufei.han <shufei.han@gl-inet.com>
2024-11-25 16:46:11 +0800

    feat: 新增状态显示

--- src/models/session.model.ts
+++ src/models/session.model.ts
@@ -12,200 +12,200 @@ import { $ } from "./janus.model";
 // @ts-ignore
 const __ascii_encoder = new TextEncoder("ascii");
 
-export class Session {
-    public ws: WebSocket
-    public missed_heartbeats = 0
-    // public streamer = new Streamer()
-    constructor() {
-        this.startSession()
-    }
+// export class Session {
+//     public ws: WebSocket
+//     public missed_heartbeats = 0
+//     // public streamer = new Streamer()
+//     constructor() {
+//         this.startSession()
+//     }
 
-    private startSession() {
-        const ws = new WebSocketService('/api/ws').socket
-        ws.sendHidedEvent = (event) => this.__sendHidEvent(event.event_type, event.event)
-        ws.onopen = this.wsOpenHandler
-        ws.onmessage = this.wsMessageHandler
-        this.ws = ws
-    }
+//     private startSession() {
+//         const ws = new WebSocketService('/api/ws').socket
+//         ws.sendHidedEvent = (event) => this.__sendHidEvent(event.event_type, event.event)
+//         ws.onopen = this.wsOpenHandler
+//         ws.onmessage = this.wsMessageHandler
+//         this.ws = ws
+//     }
 
-    private __sendHidEvent(event_type, event) {
-        const { ws } = this
-        if (event_type == "key") {
-            let data = __ascii_encoder.encode("\x01\x00" + event.key);
-            data[1] = (event.state ? 1 : 0);
-            ws.send(data);
+//     private __sendHidEvent(event_type, event) {
+//         const { ws } = this
+//         if (event_type == "key") {
+//             let data = __ascii_encoder.encode("\x01\x00" + event.key);
+//             data[1] = (event.state ? 1 : 0);
+//             ws.send(data);
 
-        } else if (event_type == "mouse_button") {
-            let data = __ascii_encoder.encode("\x02\x00" + event.button);
-            data[1] = (event.state ? 1 : 0);
-            ws.send(data);
+//         } else if (event_type == "mouse_button") {
+//             let data = __ascii_encoder.encode("\x02\x00" + event.button);
+//             data[1] = (event.state ? 1 : 0);
+//             ws.send(data);
 
-        } else if (event_type == "mouse_move") {
-            let data = new Uint8Array([
-                3,
-                (event.to.x >> 8) & 0xFF, event.to.x & 0xFF,
-                (event.to.y >> 8) & 0xFF, event.to.y & 0xFF,
-            ]);
-            ws.send(data);
+//         } else if (event_type == "mouse_move") {
+//             let data = new Uint8Array([
+//                 3,
+//                 (event.to.x >> 8) & 0xFF, event.to.x & 0xFF,
+//                 (event.to.y >> 8) & 0xFF, event.to.y & 0xFF,
+//             ]);
+//             ws.send(data);
 
-        } else if (event_type == "mouse_relative" || event_type == "mouse_wheel") {
-            let data;
-            if (Array.isArray(event.delta)) {
-                data = new Int8Array(2 + event.delta.length * 2);
-                let index = 0;
-                for (let delta of event.delta) {
-                    data[index + 2] = delta["x"];
-                    data[index + 3] = delta["y"];
-                    index += 2;
-                }
-            } else {
-                data = new Int8Array([0, 0, event.delta.x, event.delta.y]);
-            }
-            data[0] = (event_type == "mouse_relative" ? 4 : 5);
-            data[1] = (event.squash ? 1 : 0);
-            ws.send(data);
-        }
-    }
+//         } else if (event_type == "mouse_relative" || event_type == "mouse_wheel") {
+//             let data;
+//             if (Array.isArray(event.delta)) {
+//                 data = new Int8Array(2 + event.delta.length * 2);
+//                 let index = 0;
+//                 for (let delta of event.delta) {
+//                     data[index + 2] = delta["x"];
+//                     data[index + 3] = delta["y"];
+//                     index += 2;
+//                 }
+//             } else {
+//                 data = new Int8Array([0, 0, event.delta.x, event.delta.y]);
+//             }
+//             data[0] = (event_type == "mouse_relative" ? 4 : 5);
+//             data[1] = (event.squash ? 1 : 0);
+//             ws.send(data);
+//         }
+//     }
 
-    private wsMessageHandler(event) {
-        // tools.debug("Session: received socket data:", event.data);
-        let data = JSON.parse(event.data);
-        switch (data.event_type) {
-            case "pong": this.missed_heartbeats = 0; break;
-            case "info_meta_state": this.setAboutInfoMeta(data.event); break;
-            case "info_hw_state": this.setAboutInfoHw(data.event); break;
-            case "info_fan_state": this.setAboutInfoFan(data.event); break;
-            case "info_system_state": this.setAboutInfoSystem(data.event); break;
-            case "info_extras_state": this.setExtras(data.event); break;
-            // 这两种是GPIO消息，先不处理，后续搞清楚了再处理
-            // case "gpio_model_state": this.gpio.setModel(data.event); break;
-            // case "gpio_state": this.gpio.setState(data.event); break;
-            // case "hid_keymaps_state": this.hid.setKeymaps(data.event); break;
-            // case "hid_state": this.hid.setState(data.event); break;
-            // case "atx_state": this.atx.setState(data.event); break;
-            // case "msd_state": this.msd.setState(data.event); break;
-            case "streamer_state": this.streamer.setState(data.event); break;
-            // case "streamer_ocr_state": this.ocr.setState(data.event); break;
-        }
-    }
+//     private wsMessageHandler(event) {
+//         // tools.debug("Session: received socket data:", event.data);
+//         let data = JSON.parse(event.data);
+//         switch (data.event_type) {
+//             case "pong": this.missed_heartbeats = 0; break;
+//             case "info_meta_state": this.setAboutInfoMeta(data.event); break;
+//             case "info_hw_state": this.setAboutInfoHw(data.event); break;
+//             case "info_fan_state": this.setAboutInfoFan(data.event); break;
+//             case "info_system_state": this.setAboutInfoSystem(data.event); break;
+//             case "info_extras_state": this.setExtras(data.event); break;
+//             // 这两种是GPIO消息，先不处理，后续搞清楚了再处理
+//             // case "gpio_model_state": this.gpio.setModel(data.event); break;
+//             // case "gpio_state": this.gpio.setState(data.event); break;
+//             // case "hid_keymaps_state": this.hid.setKeymaps(data.event); break;
+//             // case "hid_state": this.hid.setState(data.event); break;
+//             // case "atx_state": this.atx.setState(data.event); break;
+//             // case "msd_state": this.msd.setState(data.event); break;
+//             case "streamer_state": this.streamer.setState(data.event); break;
+//             // case "streamer_ocr_state": this.ocr.setState(data.event); break;
+//         }
+//     }
 
-    private wsOpenHandler(event) {
-        console.log('WS_ON_OPEN');
+//     private wsOpenHandler(event) {
+//         console.log('WS_ON_OPEN');
 
-        // tools.debug("Session: socket opened:", event);
-        // $("link-led").className = "led-green";
-        // $("link-led").title = "Connected";
-        // __recorder.setSocket(__ws);
-        // __hid.setSocket(__ws);
-        // __missed_heartbeats = 0;
-        // __ping_timer = setInterval(__pingServer, 1000);
-    }
+//         // tools.debug("Session: socket opened:", event);
+//         // $("link-led").className = "led-green";
+//         // $("link-led").title = "Connected";
+//         // __recorder.setSocket(__ws);
+//         // __hid.setSocket(__ws);
+//         // __missed_heartbeats = 0;
+//         // __ping_timer = setInterval(__pingServer, 1000);
+//     }
 
-    private setAboutInfoMeta(state) {
-        if (state !== null) {
-            let text = JSON.stringify(state, undefined, 4).replace(/ /g, "&nbsp;").replace(/\n/g, "<br>");
-            $("about-meta").innerHTML = `
-				<span class="code-comment">// The PiKVM metadata.<br>
-				// You can get this JSON using handle <a target="_blank" href="/api/info?fields=meta">/api/info?fields=meta</a>.<br>
-				// In the standard configuration this data<br>
-				// is specified in the file /etc/kvmd/meta.yaml.</span><br>
-				<br>
-				${text}
-			`;
-            if (state.server && state.server.host) {
-                $("kvmd-meta-server-host").innerHTML = `Server: ${state.server.host}`;
-                document.title = `PiKVM Session: ${state.server.host}`;
-            } else {
-                $("kvmd-meta-server-host").innerHTML = "";
-                document.title = "PiKVM Session";
-            }
+//     private setAboutInfoMeta(state) {
+//         if (state !== null) {
+//             let text = JSON.stringify(state, undefined, 4).replace(/ /g, "&nbsp;").replace(/\n/g, "<br>");
+//             $("about-meta").innerHTML = `
+// 				<span class="code-comment">// The PiKVM metadata.<br>
+// 				// You can get this JSON using handle <a target="_blank" href="/api/info?fields=meta">/api/info?fields=meta</a>.<br>
+// 				// In the standard configuration this data<br>
+// 				// is specified in the file /etc/kvmd/meta.yaml.</span><br>
+// 				<br>
+// 				${text}
+// 			`;
+//             if (state.server && state.server.host) {
+//                 $("kvmd-meta-server-host").innerHTML = `Server: ${state.server.host}`;
+//                 document.title = `PiKVM Session: ${state.server.host}`;
+//             } else {
+//                 $("kvmd-meta-server-host").innerHTML = "";
+//                 document.title = "PiKVM Session";
+//             }
 
-            // Don't use this option, it may be removed in any time
-            if (state.web && state.web.confirm_session_exit === false) {
-                window.onbeforeunload = null; // See main.js
-            }
-        }
-    }
+//             // Don't use this option, it may be removed in any time
+//             if (state.web && state.web.confirm_session_exit === false) {
+//                 window.onbeforeunload = null; // See main.js
+//             }
+//         }
+//     }
 
-    private setAboutInfoHw(state) {
-        // if (state.health.throttling !== null) {
-        // 	let flags = state.health.throttling.parsed_flags;
-        // 	let ignore_past = state.health.throttling.ignore_past;
-        // 	let undervoltage = (flags.undervoltage.now || (flags.undervoltage.past && !ignore_past));
-        // 	let freq_capped = (flags.freq_capped.now || (flags.freq_capped.past && !ignore_past));
+//     private setAboutInfoHw(state) {
+//         // if (state.health.throttling !== null) {
+//         // 	let flags = state.health.throttling.parsed_flags;
+//         // 	let ignore_past = state.health.throttling.ignore_past;
+//         // 	let undervoltage = (flags.undervoltage.now || (flags.undervoltage.past && !ignore_past));
+//         // 	let freq_capped = (flags.freq_capped.now || (flags.freq_capped.past && !ignore_past));
 
-        // 	tools.hidden.setVisible($("hw-health-dropdown"), (undervoltage || freq_capped));
-        // 	$("hw-health-undervoltage-led").className = (undervoltage ? (flags.undervoltage.now ? "led-red" : "led-yellow") : "hidden");
-        // 	$("hw-health-overheating-led").className = (freq_capped ? (flags.freq_capped.now ? "led-red" : "led-yellow") : "hidden");
-        // 	tools.hidden.setVisible($("hw-health-message-undervoltage"), undervoltage);
-        // 	tools.hidden.setVisible($("hw-health-message-overheating"), freq_capped);
-        // }
-        // __info_hw_state = state;
-        // __renderAboutInfoHardware();
-    };
+//         // 	tools.hidden.setVisible($("hw-health-dropdown"), (undervoltage || freq_capped));
+//         // 	$("hw-health-undervoltage-led").className = (undervoltage ? (flags.undervoltage.now ? "led-red" : "led-yellow") : "hidden");
+//         // 	$("hw-health-overheating-led").className = (freq_capped ? (flags.freq_capped.now ? "led-red" : "led-yellow") : "hidden");
+//         // 	tools.hidden.setVisible($("hw-health-message-undervoltage"), undervoltage);
+//         // 	tools.hidden.setVisible($("hw-health-message-overheating"), freq_capped);
+//         // }
+//         // __info_hw_state = state;
+//         // __renderAboutInfoHardware();
+//     };
 
-    private setAboutInfoFan(state) {
-        let failed = false;
-        let failed_past = false;
-        if (state.monitored) {
-            if (state.state === null) {
-                failed = true;
-            } else {
-                if (!state.state.fan.ok) {
-                    failed = true;
-                } else if (state.state.fan.last_fail_ts >= 0) {
-                    failed = true;
-                    failed_past = true;
-                }
-            }
-        }
-        // tools.hidden.setVisible($("fan-health-dropdown"), failed);
-        // $("fan-health-led").className = (failed ? (failed_past ? "led-yellow" : "led-red") : "hidden");
+//     private setAboutInfoFan(state) {
+//         let failed = false;
+//         let failed_past = false;
+//         if (state.monitored) {
+//             if (state.state === null) {
+//                 failed = true;
+//             } else {
+//                 if (!state.state.fan.ok) {
+//                     failed = true;
+//                 } else if (state.state.fan.last_fail_ts >= 0) {
+//                     failed = true;
+//                     failed_past = true;
+//                 }
+//             }
+//         }
+//         // tools.hidden.setVisible($("fan-health-dropdown"), failed);
+//         // $("fan-health-led").className = (failed ? (failed_past ? "led-yellow" : "led-red") : "hidden");
 
-        // __info_fan_state = state;
-        // __renderAboutInfoHardware();
-    };
+//         // __info_fan_state = state;
+//         // __renderAboutInfoHardware();
+//     };
 
-    private setAboutInfoSystem = function (state) {
-        console.log('setAboutInfoSystem', state);
-        // $("about-version").innerHTML = `
-        // 	KVMD: <span class="code-comment">${state.kvmd.version}</span><br>
-        // 	<hr>
-        // 	Streamer: <span class="code-comment">${state.streamer.version} (${state.streamer.app})</span>
-        // 	${__formatStreamerFeatures(state.streamer.features)}
-        // 	<hr>
-        // 	${state.kernel.system} kernel:
-        // 	${__formatUname(state.kernel)}
-        // `;
-        // $("kvmd-version-kvmd").innerHTML = state.kvmd.version;
-        // $("kvmd-version-streamer").innerHTML = state.streamer.version;
-    }
+//     private setAboutInfoSystem = function (state) {
+//         console.log('setAboutInfoSystem', state);
+//         // $("about-version").innerHTML = `
+//         // 	KVMD: <span class="code-comment">${state.kvmd.version}</span><br>
+//         // 	<hr>
+//         // 	Streamer: <span class="code-comment">${state.streamer.version} (${state.streamer.app})</span>
+//         // 	${__formatStreamerFeatures(state.streamer.features)}
+//         // 	<hr>
+//         // 	${state.kernel.system} kernel:
+//         // 	${__formatUname(state.kernel)}
+//         // `;
+//         // $("kvmd-version-kvmd").innerHTML = state.kvmd.version;
+//         // $("kvmd-version-streamer").innerHTML = state.streamer.version;
+//     }
 
-    private setExtras(state) {
-        // let show_hook = null;
-        // let close_hook = null;
-        // let has_webterm = (state.webterm && (state.webterm.enabled || state.webterm.started));
-        // if (has_webterm) {
-        // 	let path = "/" + state.webterm.path + "?disableLeaveAlert=true";
-        // 	show_hook = function() {
-        // 		tools.info("Terminal opened: ", path);
-        // 		$("webterm-iframe").src = path;
-        // 	};
-        // 	close_hook = function() {
-        // 		tools.info("Terminal closed");
-        // 		$("webterm-iframe").src = "";
-        // 	};
-        // }
-        // tools.feature.setEnabled($("system-tool-webterm"), has_webterm);
-        // $("webterm-window").show_hook = show_hook;
-        // $("webterm-window").close_hook = close_hook;
-        // console.log("SET_JANUS_ENABLED: ", state);
-        // __streamer.setJanusEnabled(
-        // 	(state.janus && (state.janus.enabled || state.janus.started))
-        // 	|| (state.janus_static && (state.janus_static.enabled || state.janus_static.started))
-        // );
-    }
-}
+//     private setExtras(state) {
+//         // let show_hook = null;
+//         // let close_hook = null;
+//         // let has_webterm = (state.webterm && (state.webterm.enabled || state.webterm.started));
+//         // if (has_webterm) {
+//         // 	let path = "/" + state.webterm.path + "?disableLeaveAlert=true";
+//         // 	show_hook = function() {
+//         // 		tools.info("Terminal opened: ", path);
+//         // 		$("webterm-iframe").src = path;
+//         // 	};
+//         // 	close_hook = function() {
+//         // 		tools.info("Terminal closed");
+//         // 		$("webterm-iframe").src = "";
+//         // 	};
+//         // }
+//         // tools.feature.setEnabled($("system-tool-webterm"), has_webterm);
+//         // $("webterm-window").show_hook = show_hook;
+//         // $("webterm-window").close_hook = close_hook;
+//         // console.log("SET_JANUS_ENABLED: ", state);
+//         // __streamer.setJanusEnabled(
+//         // 	(state.janus && (state.janus.enabled || state.janus.started))
+//         // 	|| (state.janus_static && (state.janus_static.enabled || state.janus_static.started))
+//         // );
+//     }
+// }
 
 export function sendHidEvent(ws, { event_type, event }) {
     if (event_type == "key") {
--- src/views/kvm/KvmPageHeader.vue
+++ src/views/kvm/KvmPageHeader.vue
@@ -2,15 +2,20 @@
  * @Author: shufei.han
  * @Date: 2024-10-16 10:26:33
  * @LastEditors: shufei.han
- * @LastEditTime: 2024-10-16 17:10:09
+ * @LastEditTime: 2024-11-25 16:32:51
  * @FilePath: \kvm-web-vue3\src\views\kvm\KvmPageHeader.vue
  * @Description: 
 -->
 <template>
     <div class="kvm-page-header flex-btw">
         <KvmLogo class="pointer" />
-
         <div class="flex">
+            <div class="info-box flex-start">
+                <a-tooltip :title="kvmStore.streamStateTitle">
+                    <img :src="linkSvgSrc" :class="{'link-img': true, 'img-enabled': msgStore?.connected}" alt="">
+                </a-tooltip>
+                <img :src="streamSvgSrc" :class="{'link-img': true, 'stream-status': true, 'img-enabled': kvmStore.streamState}" alt="">
+            </div>
             <a-radio-group button-style="solid" :value="mode" @change="handleChange">
                 <a-radio-button :value="VideoStreamEnum.MJPEG">MJPEG</a-radio-button>
                 <a-radio-button :value="VideoStreamEnum.WEBRTC">WEBRTC</a-radio-button>
@@ -23,8 +28,14 @@
 import KvmLogo from '@/components/KvmLogo.vue';
 import { VideoStreamEnum } from '@/models/kvm.model';
 import { RadioChangeEvent } from 'ant-design-vue';
+import linkSvgSrc from '@/assets/imgs/led-link.svg'
+import streamSvgSrc from '@/assets/imgs/led-stream.svg'
+import { useMsgStore } from '@/stores/message';
+import { useKvmStore } from '@/stores/kvm';
 
 defineProps<{mode: VideoStreamEnum}>();
+const msgStore = useMsgStore()
+const kvmStore = useKvmStore()
 
 const emits = defineEmits<{
     (e: 'update:mode', mode: VideoStreamEnum): void;
@@ -45,5 +56,19 @@ const handleChange = (ev:RadioChangeEvent) => {
     position: fixed;
     width: 100%;
     box-sizing: border-box;
+    .info-box {
+        .link-img {
+            width: 20px;
+            height: 20px;
+            margin-right: 8px;
+            filter: invert(0.5) sepia(1) saturate(5) hue-rotate(0deg)
+        }
+        .stream-status {
+            filter: invert(0.5);    
+        }
+        .img-enabled {
+            filter:invert(0.5) sepia(1) saturate(5) hue-rotate(100deg);
+        }
+    }
 }
 </style>
